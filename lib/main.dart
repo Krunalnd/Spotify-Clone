2f779a8060f6a308178398111efd1dc97d768aa7
Krunal Darji <126603729+Krunalnd@users.noreply.github.com>
2025-03-19 11:24:32 +0530

    title name
    change title name

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:spotify_clone/domain/app_routes.dart';
 import 'package:spotify_clone/ui/dashboard/dashboard_screen.dart';
-import 'ui/splash/splash_page.dart';
-
 void main() {
   runApp(const MyApp());
 }
@@ -13,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Spotify Clone',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
